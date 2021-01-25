dbbe02ab28535d1bb1882449d1ba20aa2cce2e2e
Gabriel Harris <gch910@gmail.com>
2021-01-25 15:14:44 -0600

    on phase 3

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.set('view engine', 'pug');
 app.use(morgan('dev'));
 app.use(routes);
 
+
+
 app.use( (req, res, next) => {
    const err = new Error('The requested page couldn\'t be found!');
    err.status = 404;
@@ -21,6 +23,31 @@ app.use( (err, req, res, next) => {
    }
    next(err);
 })
+
+app.use((err, req, res, next) => {
+   if(err.status === 404) {
+      res.status(404)
+      res.render('page-not-found', {
+         title: 'Page Not Found'
+      })
+   } else {
+      next(err)
+   }
+   
+})
+
+app.use((err, req, res, next) => {
+   res.status(err.status || 500)
+   const isProduction = process.env.NODE_ENV === 'production';
+   res.render('error', {
+      title: 'Server Error',
+      message: isProduction ? null : err.message,
+      stack: isProduction ? null : err.stack,
+   })
+
+})
+
+
 
 
 
--- routes.js
+++ routes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
+
+
 router.get('/', asyncHandler( async (req, res) => {
    res.render('index', {title: 'Home'})
 }))
+
 
 module.exports = router;
