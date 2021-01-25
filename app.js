ac94371e564de445008617f541c7fa325476aa87
Matt Johnston <emollientspy1@gmail.com>
2021-01-25 12:31:20 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const app = express();
+app.set('view engine', 'pug');
+
+app.use(morgan('dev'));
+app.use(routes);
+
+app.use( (req, res, next) => {
+   const err = new Error('The requested page couldn\'t be found!');
+   err.status = 404;
+   next(err)
+});
+
+app.use( (err, req, res, next) => {
+   if (process.env.NODE_ENV === 'production') {
+   } else {
+      console.error(err);
+   }
+   next(err);
+})
+
+
+
+module.exports = app;
---
+++ routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/', asyncHandler( async (req, res) => {
+   res.render('index', {title: 'Home'})
+}))
+
+module.exports = router;
